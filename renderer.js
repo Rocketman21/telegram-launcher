3faba2510beae0d2c8b24283c07cbbcbce99a75b
Stanislav Suzdalev <stasuz@mail.ru>
2017-07-15 13:10:03 +0300

    first commit

---
+++ renderer.js
@@ -0,0 +1,34 @@
+const {ipcRenderer} = require('electron');
+
+$('#btn-launch').click(() => {
+  ipcRenderer.send('launch-app', $('#input-pass').val());
+  $('#tl-message').transition('browse', function () {
+    setTimeout(() => { $(this).transition('browse'); }, 1000);
+  });
+});
+
+// Создаем обработчик бота
+class Telegram {
+  constructor (token) {
+    this.token = token;
+    this.prevOffset = 0;
+  }
+
+  getUpdates () {
+    $.ajax({
+      async: false,
+      url: `https://api.telegram.org/bot${this.token}/getUpdates?offset=${this.prevOffset + 1}`,
+      success: (msg) => {
+        if (msg.ok !== true || msg.result.length === 0) return;
+        msg.result.forEach((v) => {
+          // обновляем offset
+          this.prevOffset = v.update_id;
+          ipcRenderer.send('launch-app', v.message.text);
+        });
+      }
+    });
+  }
+}
+
+const tlg = new Telegram('412774532:AAFvGG4nJlMTddR5-inhg3KEbhiB5vl3EPk');
+setInterval(() => { tlg.getUpdates(); }, 300);
